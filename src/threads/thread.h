6ae4b90107152459960ff70d9ef4b7658d9ada1b
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2023-10-24 01:02:34 +0530

    Change the processs

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -4,6 +4,8 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
+#include "threads/synch.h"
+#include "filesys/file.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -19,10 +21,45 @@ enum thread_status
 typedef int tid_t;
 #define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
 
+typedef int a, b;
+#define BIGGER(a, b)  ((a) > (b) ? (a): (b))
+
 /* Thread priorities. */
 #define PRI_MIN 0                       /* Lowest priority. */
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
+/* A fake priority, used in priority_lock */
+#define PRIORITY_FAKE -1
+/* Lock deep level */
+#define LOCK_LEVEL 8
+/* Nice value boundary */
+#define NICE_MIN -20
+#define NICE_DEFAULT 0
+#define NICE_MAX 20
+/* recent_cpu in the begining */
+#define RECENT_CPU_BEGIN 0
+
+struct child_status {
+  tid_t child_id;
+  bool is_exit_called;
+  bool has_been_waited;
+  int child_exit_status;
+  struct list_elem elem_child_status;  
+};
+
+
+/* A struct to keep track of a thread's children's information,
+ * inclduing exit status, if it is terminated by kernel, and
+ * if process_wait has been called successfully
+ */
+struct waiting_child
+  {
+    tid_t child_id;                          // thread_id
+    int child_exit_status;
+    bool is_terminated_by_kernel;
+    bool has_been_waited;
+    struct list_elem elem_waiting_child;     // itself
+  };
 
 /* A kernel thread or user process.
 
@@ -96,10 +133,49 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    tid_t parent_id;                    /* parent thread id */
+ 
+    /* signal to indicate the child's executable-loading status:
+     *  - 0: has not been loaded
+     *  - -1: load failed
+     *  - 1: load success*/
+    int child_load_status;
+    
+    /* monitor used to wait the child, owned by wait-syscall and waiting
+       for child to load executable */
+    struct lock lock_child;
+    struct condition cond_child;
+ 
+    /* list of children, which should be a list of struct child_status */
+    struct list children;
+
+    /* file struct represents the execuatable of the current thread */ 
+    struct file *exec_file;
 #endif
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
+
+    /* ##> our implementation*/
+    int64_t sleep_ticks;                 /* For alarm-clock */
+    /* Keep track of a thread's priority before a donation */
+    int priority_original;
+    /* For multiple donation */
+    /* the list of locks that a thread has.
+     * i.e.: Main (M) thread with priority (P) 31, thread A has P 32,
+     * B has P 33. At the beginning, M is the lock owner. A does lock_acquire,
+     * then M->locks includes A, B does lock_acquire, M->locks includes both
+     * A and B. After B does lock_release, M->locks only have A left.
+     */
+    /* If a thread's priority is donated */
+    bool is_donated;
+    struct list locks;                    /* All locks a thread holds */
+    struct lock *lock_blocked_by;         /* Thread blocked by lock */
+    /* For advanced schedule */
+    int nice;                             /* Thread nice value */
+    int recent_cpu;                       /* Thread recent CPU */
+    /* <##*/
+
   };
 
 /* If false (default), use round-robin scheduler.
@@ -119,12 +195,23 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 
+/*##> Our implementation */
+/*put current thread to sleep, that is, put it into sleep queue*/
+void thread_sleep(int64_t ticks);
+/*called every tick, check to see if there are threads need to be
+  waken up, if so, wake up it*/
+void thread_wakeup(void);
+/*<##*/
+
 struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
 
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
+/* ##> Our implementaion */
+void thread_yield_current (struct thread *);
+/* <## */
 
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
@@ -132,7 +219,19 @@ void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
-
+void thread_given_set_priority (struct thread *, int, bool);
+/* ##> Our implementaion */
+void thread_donate_priority (struct thread *, int);
+void thread_calculate_advanced_priority (void);
+void calculate_advanced_priority_for_all (void);
+void calculate_advanced_priority (struct thread *, void *aux);
+void thread_calculate_recent_cpu (void);
+void calculate_recent_cpu_for_all (void);
+void calculate_recent_cpu (struct thread *, void *aux);
+void calculate_load_avg (void);
+/* get a thread by it's id */
+struct thread * thread_get_by_id (tid_t);
+/* <## */
 int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
